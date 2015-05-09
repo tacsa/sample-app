cd5597a2ad3ed146181d50b432a94b5259d2f060
Tac Sanada <tacsana@yahoo.co.jp>
2015-05-09 21:15:05 +0900

    test

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -11,13 +11,13 @@
 # if you're sharing your code publicly.
 require 'securerandom'
 
-def seure_token
+def secure_token
   token_file = Rails.root.join('.secret')
   if File.exist?(token_file)
-    #Use the existing token.
+    # Use the existing token.
     File.read(token_file).chomp
   else
-    #Generate a new token and store it in token_file
+    # Generate a new token and store it in token_file.
     token = SecureRandom.hex(64)
     File.write(token_file, token)
     token
