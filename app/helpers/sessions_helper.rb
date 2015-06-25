77e4a11c0d43eb21474c43dd33a2134ad4f7e478
Tac Sanada <tacsana@yahoo.co.jp>
2015-06-25 09:27:37 +0900

    Add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,12 +18,19 @@ module SessionsHelper
   def current_user
   remember_token = User.encrypt(cookies[:remember_token])
   @current_user ||= User.find_by(remember_token: remember_token)
-end
+  end
 
-def current_user?(user)
+  def current_user?(user)
   user == current_user
-end
+  end
 
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+  
   def sign_out
     self.current_user = nil
     cookies.delete(:remember_token)
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -91,6 +91,19 @@ end
         it { should have_title('Sign in') }
       end
 
+      describe "in the Microposts controller" do
+
+      describe "submitting to the create action" do
+        before { post microposts_path }
+        specify { expect(response).to redirect_to(signin_path) }
+      end
+
+      describe "submitting to the destroy action" do
+        before { delete micropost_path(FactoryGirl.create(:micropost)) }
+        specify { expect(response).to redirect_to(signin_path) }
+      end
+    end
+
       describe "submitting to the update action" do
         before { patch user_path(user) }
         specify { expect(response).to redirect_to(signin_path) }
